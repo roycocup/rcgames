105bf52058b5cc6bca2a918b7d0d142f745eb8a3
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-08-03 18:24:11 +0100

    first blood

---
+++ wp-config.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'rcgames');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '&i<`hYH-ew8nMI/U9vfTU5w/F2!]cQV`S]LWfud[dG$5U~(ll|yj(_Jwtzk/,DF`');
+define('SECURE_AUTH_KEY',  'R3n]92S?;%=,EqM9X!`s7|u6K}jRwabDHXI6@nV^F!q8oOrlzS30box3A*qvC@WG');
+define('LOGGED_IN_KEY',    'qT0yr1 &eC2H@5O7?m3b<N`~eVi8s3kjDz#YQ(*gHX[4C5}YK6cDK.<,*ek42B9(');
+define('NONCE_KEY',        'Ri3[]wy6NxHMj/^pQ]|1gCVyrb@Zc,)S`fL}EUHsnF9lYq|iiZ0e<.qFG9Ak-4JZ');
+define('AUTH_SALT',        '|M$iZ]=l!ddrpd8l!iT[V</=BFU2A?l}dBTsK2=g62Y;IV[;~?ybmcK jr/2,9LT');
+define('SECURE_AUTH_SALT', 'muPng,+*Q.-p12.o?[JTTaxOIgs280tI4v)DWQ kMHK[j]jzF:BS;7KBPwMj4*Y#');
+define('LOGGED_IN_SALT',   'TwasjM4!k4[:s#LF-T`OV!1S; I`?8%<N 3<?_n&s]mGH~ne#_IuC&XT+MqlaFMI');
+define('NONCE_SALT',       '!%+Q/WKOri,gfvZv9RB+jSW@NXvx`#+w^W2eg@Zys~Wz^[ube0Ru0yEi,J0p&O=`');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/amigo1985-master/README.md
@@ -0,0 +1,35 @@
+amigo1985
+=========
+
+Amigo 1985 WordPress Theme - a tribute to Amiga Workbench 1.x
+
+This WordPress theme started as one of my school projects in 2011, and I wanted to share it.
+
+http://www.lizdizon.com/amigo1985/
+
+features
+--------
+
+* Makes your WordPress installation look like Amiga Workbench 1.x
+* Widget support
+
+quick install
+-------------
+
+1. [Download the amigo1985 project as a .zip](https://github.com/lizdizon/amigo1985/archive/master.zip) and unarchive it into the `/wp-content/themes/` directory of your WordPress installation.
+  * You can delete the .psd files in `/images/` before uploading the theme to your server in order to save upload time and drive space. They are only there in case you needed to modify the theme images.
+  * Optionally, rename the new theme's directory to `amigo1985`.
+2. Activate the theme from your WordPress Dashboard > Appearance > Themes.
+
+known issues
+------------
+
+* Terse documentation: I'm currently attending university and do not have all the time I want for all my projects, so I've put this Amiga theme on GitHub for all the fans who have encouraged me to do so (thank you for the moral support!).
+* **Please post theme issues in the [project issue tracker](https://github.com/lizdizon/amigo1985/issues), and I welcome anyone to contribute to the project and make it a better theme!**
+
+dev caveat
+-----------
+* The theme uses the [960 grid system](http://960.gs/) (it was part of the original project spec), so you'll want to familiarize yourself with that if you're going to be hacking divs.
+
+— Liz Dizon
+6 February 2013
